2c648ec6ac1a992f1652683d04102f43712e5aca
0VitorGabriel <victoropo64@outlook.com>
2023-05-05 16:24:44 -0400

    funcao de mostrar as horas, minutos e segundos

--- script/script.js
+++ script/script.js
@@ -2,19 +2,39 @@ const btn_start = document.querySelector('.btn_comecar')
 
 const area_seconds = document.querySelector('.segundos')
 
+const area_minutes = document.querySelector('.minutos')
+
+const area_hours = document.querySelector('.horas')
+
 let seconds = 0
+let minutes = 0
+let hours = 0
 
-btn_start.addEventListener('click', show_seconds)
+btn_start.addEventListener('click', show_temp)
 
-function show_seconds() {
-    if (seconds == 60) {
+function show_temp() {
+    if (seconds == 59) {
         seconds = 0
-    } else {
-        seconds += 1
-        area_seconds.innerHTML = seconds
-    
-        setTimeout('show_seconds()', 1000)
-    }
+
+        if (minutes == 59) {
+            minutes = 0
+            hours += 1
+
+            area_hours.innerHTML = hours
+
+            area_minutes.innerHTML = minutes
+        } else {
+
+            minutes += 1 
+
+            area_minutes.innerHTML = minutes
+        }
+    } 
+
+    seconds += 1
+    area_seconds.innerHTML = seconds
+
+    setTimeout('show_temp()', 1000)
 }
 
 
